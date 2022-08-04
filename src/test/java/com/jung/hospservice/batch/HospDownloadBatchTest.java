36348b8d2a0c50331f190571ccfb6dec1895e64c
ap3334 <chwil2989@naver.com>
2022-08-04 16:02:22 +0900

    공공데이터 불러오기 테스트 이름 수정

--- src/test/java/com/jung/hospservice/batch/HospDownloadBatchTest.java
+++ src/test/java/com/jung/hospservice/batch/HospDownloadBatchTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class HospDownloadBatchTest {
 
     @Test
-    public void start() {
+    public void 공공데이터_다운로드_테스트() {
         RestTemplate rt = new RestTemplate();
         // 테스트시에는 인증키 인코딩 안해주어도 됨 + PKIX 오류 발생으로 인해 임시적으로 https -> http로 수정
         String url = "http://apis.data.go.kr/B551182/rprtHospService/getRprtHospService?serviceKey=/7bUQRDAVeOz8OUi2pk6QLOFXx/gJTQMKFoym8SP18azscytoTJqYPSPj7TpDNy7gRyJNRIaqR4l0tp9vgPuFw==&pageNo=1&numOfRows=10&_type=json";
