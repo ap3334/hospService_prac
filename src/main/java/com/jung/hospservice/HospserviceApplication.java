848c694d48ef044987f95800fdbf708649b27273
ap3334 <chwil2989@naver.com>
2022-08-04 14:02:19 +0900

    초기 세팅

---
+++ src/main/java/com/jung/hospservice/HospserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jung.hospservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jung/hospservice/HospserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jung.hospservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
