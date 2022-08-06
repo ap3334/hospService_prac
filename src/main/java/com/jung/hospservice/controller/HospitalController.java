915cf49df90f6ed9e42632fcc2c99224e86db085
ap3334 <chwil2989@naver.com>
2022-08-06 17:28:36 +0900

    view에서 병원 검색 기능 추가

--- src/main/java/com/jung/hospservice/controller/HospitalController.java
+++ src/main/java/com/jung/hospservice/controller/HospitalController.java
@@ -1,20 +1,39 @@
 package com.jung.hospservice.controller;
 
+import com.jung.hospservice.dto.HospitalDto;
 import com.jung.hospservice.repository.HospitalRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+@Log4j2
 @RequiredArgsConstructor
 @Controller
 public class HospitalController {
 
-    private HospitalRepository hospitalRepository;
+    private final HospitalRepository hospitalRepository;
 
     @GetMapping("/home")
     public String home() {
+
         return "index";
     }
+
+    @PostMapping("/home")
+    public String showHospital(String sidoCdNm, String sgguCdNm, Model model) {
+
+        List<HospitalDto> hospitals = hospitalRepository.mFindHospital(sidoCdNm, sgguCdNm);
+
+        model.addAttribute("hospitals", hospitals);
+
+        return "showHospital";
+    }
+
 
 }
 
--- src/main/java/com/jung/hospservice/repository/HospitalRepository.java
+++ src/main/java/com/jung/hospservice/repository/HospitalRepository.java
@@ -2,12 +2,17 @@ package com.jung.hospservice.repository;
 
 import com.jung.hospservice.dto.HospitalDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface HospitalRepository extends JpaRepository<HospitalDto, Long> {
 
-
+    @Query(value = "SELECT * from hospitaldto where sidoCdNm =:sidoCdNm and sgguCdNm = :sgguCdNm", nativeQuery = true)
+    List<HospitalDto> mFindHospital(@Param("sidoCdNm") String sidoCdNm, @Param("sgguCdNm") String sgguCdNm);
 
 
 }
